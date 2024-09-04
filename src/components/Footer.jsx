def138ee04dfa7a0abdeba45310ba32db92a413f
LiyanaKhalid <liyanakhalid1198@gmail.com>
2024-09-04 20:51:02 +0530

    restaurant project thunk

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+
+function Footer() {
+  return (
+    <>
+    <Row>
+        <Col className='text-center py-5'>
+        <p>All Rights reserved © Food Circle</p>
+        </Col>
+    </Row>
+    </>
+  )
+}
+
+export default Footer
